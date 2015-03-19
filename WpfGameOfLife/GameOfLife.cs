b1dd9576caa7bb4fb4938e26d8421f5f805518bd
maciejsobotka <sobot92@tlen.pl>
2015-03-19 14:59:11 +0100

    Working game

---
+++ WpfGameOfLife/GameOfLife.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfGameOfLife
+{
+    public class GameOfLife
+    {
+        private Random random = new Random();
+        private int[,] World = new int[22, 22];        
+        private int[,] World_Future = new int[22, 22];
+        
+        public void GenerateWorld()
+        {
+            for (int i = 1; i < 21; i++)
+                for (int j = 1; j < 21; j++)
+                {
+                    World[i, j] = random.Next(0, 2);
+                }
+            World_Future = World;
+        }
+        public bool IsCellAlive(int i, int j)
+        {
+            if (World[i, j] == 0)
+                return false;
+            if (World[i, j] == 1)
+                return true;
+            return false;
+        }
+        public void Iteration()
+        {
+            int a;
+            World_Future = World;
+            for (int i = 1; i < 21; i++)
+                for (int j = 1; j < 21; j++)
+                {
+                    a = 0;
+                    a = World[i - 1, j - 1] + World[i, j - 1] + World[i + 1, j - 1] + World[i - 1, j]
+                        + World[i + 1, j] + World[i - 1, j + 1] + World[i, j + 1] + World[i + 1, j + 1];
+                    CellUpdate(i, j, a);
+                }
+            World = World_Future;
+        }
+        private void CellUpdate(int i, int j, int a)
+        {
+            if (a < 2 || a > 3)
+                World_Future[i, j] = 0;
+            if (a == 3)
+                if (World[i, j] == 0)
+                    World_Future[i, j] = 1;
+        }
+    }
+}
---
+++ WpfGameOfLife/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Windows.Threading;
+
+namespace WpfGameOfLife
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Rectangle> recList{ get; set; }
+        private GameOfLife GoL= new GameOfLife();
+        private SolidColorBrush green = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+        private SolidColorBrush white = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+        DispatcherTimer gameUpdateTimer = new DispatcherTimer();
+        DispatcherTimer iterationTimer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            recList = new List<Rectangle>();
+            for (int i = 0; i < 20; i++)
+                for (int j = 0; j < 20; j++ )
+                {
+                    recList.Add(new Rectangle { Width = 20, Height = 20, Stroke = Brushes.Red });
+                    gameArea.Children.Add(recList[20 * i + j]);
+                    Canvas.SetLeft(recList[20 * i + j], j * 20);
+                    Canvas.SetTop(recList[20 * i + j], i * 20);
+                }
+            GoL.GenerateWorld();
+            UpdateGameArea();
+
+            gameUpdateTimer.Tick += gameUpdateTimer_Tick;
+            gameUpdateTimer.Interval = TimeSpan.FromSeconds(0.0);
+
+            iterationTimer.Tick += iterationTimer_Tick;
+            iterationTimer.Interval = TimeSpan.FromSeconds(0.1);
+        }
+
+        private void gameUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            UpdateGameArea();
+            gameArea.UpdateLayout();
+        }
+        private void iterationTimer_Tick(object sender, EventArgs e)
+        {
+            PlayGame();
+        }
+        private void UpdateGameArea()
+        {
+            for (int i = 0; i < 20; i++)
+                for (int j = 0; j < 20; j++)
+                {
+                    if (GoL.IsCellAlive(i + 1, j + 1))
+                        recList[20 * i + j].Fill = green;
+                    else
+                        recList[20 * i + j].Fill = white;
+                }
+        }
+        private void PlayGame()
+        {
+            System.Threading.Thread.Sleep(500);
+            GoL.Iteration();
+        }
+        private void generateButton_Click(object sender, RoutedEventArgs e)
+        {
+            iterationTimer.Stop();
+            gameUpdateTimer.Stop();
+            GoL.GenerateWorld();
+            UpdateGameArea();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            iterationTimer.Start();
+            gameUpdateTimer.Start();
+            
+        }
+    }
+}
